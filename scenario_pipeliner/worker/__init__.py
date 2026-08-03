39442f0fdf3becd652b282ad74cf9b9040a75d01
ademidkin <ademidkin@digitlb.ru>
2026-08-03 13:14:15 +0300

    fix runtime

--- scenario_pipeliner/worker/__init__.py
+++ scenario_pipeliner/worker/__init__.py
@@ -3,3 +3,17 @@
 This package mirrors worker-side concepts so we can refactor safely
 without modifying ``src/worker`` directly.
 """
+
+from scenario_pipeliner.worker.runtime_runner import (
+    CyclicalTaskSeed,
+    build_postgres_runner,
+    create_cyclical_task,
+    ensure_worker_settings,
+)
+
+__all__ = [
+    "CyclicalTaskSeed",
+    "ensure_worker_settings",
+    "create_cyclical_task",
+    "build_postgres_runner",
+]
